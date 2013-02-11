cabbb58114cbf385f18670e4a2d322b0d12d7594
Justin Santa Barbara <justin@fathomdb.com>
2013-02-11 15:24:21 -0800

    Support paging through huge buckets

--- openstack-cli/src/main/java/org/openstack/client/cli/commands/ListFiles.java
+++ openstack-cli/src/main/java/org/openstack/client/cli/commands/ListFiles.java
@@ -1,13 +1,19 @@
 package org.openstack.client.cli.commands;
 
 import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import org.kohsuke.args4j.Argument;
 import org.openstack.client.cli.model.StoragePath;
 import org.openstack.swift.SwiftClient;
 import org.openstack.swift.api.ListObjects;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class ListFiles extends OpenstackCliCommandRunnerBase {
@@ -25,12 +31,90 @@ public class ListFiles extends OpenstackCliCommandRunnerBase {
 		String containerName = path.getContainer();
 		String objectPath = path.getObjectPath();
 
-		Map<String, String> filters = Maps.newHashMap();
-		filters.put("prefix", objectPath);
-		// filters.put("delimeter", null);
+		FileIterator iterator = new FileIterator(client, containerName,
+				objectPath, null);
+		List<org.openstack.swift.model.Object> files = Lists
+				.newArrayList(iterator);
+		return files;
+	}
 
-		ListObjects command = new ListObjects(containerName, filters);
-		return client.execute(command);
+	class FileIterator implements Iterator<org.openstack.swift.model.Object> {
+		final SwiftClient client;
+
+		final String prefix;
+		final String delimiter;
+
+		Queue<org.openstack.swift.model.Object> queue = null;
+
+		String marker;
+		boolean couldHaveMore = true;
+
+		private final String containerName;
+
+		public FileIterator(SwiftClient client, String containerName,
+				String prefix, String delimiter) {
+			super();
+			this.client = client;
+			this.containerName = containerName;
+			this.prefix = prefix;
+			this.delimiter = delimiter;
+		}
+
+		@Override
+		public boolean hasNext() {
+			if (queue == null || queue.isEmpty()) {
+				if (couldHaveMore) {
+					queue = nextPage(marker);
+				}
+				if (queue == null || queue.isEmpty()) {
+					queue = null;
+				}
+			}
+
+			return (queue != null && !queue.isEmpty());
+		}
+
+		@Override
+		public org.openstack.swift.model.Object next() {
+			if (queue == null) {
+				throw new IllegalStateException();
+			}
+
+			org.openstack.swift.model.Object so = queue.remove();
+			marker = so.getName();
+			return so;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		Queue<org.openstack.swift.model.Object> nextPage(String marker) {
+			Map<String, String> filters = Maps.newHashMap();
+			if (prefix != null) {
+				filters.put("prefix", prefix);
+			}
+
+			if (!Strings.isNullOrEmpty(delimiter)) {
+				filters.put("delimiter", delimiter);
+			}
+
+			if (!Strings.isNullOrEmpty(marker)) {
+				filters.put("marker", marker);
+			}
+
+			int limit = 10000;
+
+			ListObjects command = new ListObjects(containerName, filters);
+
+			ArrayDeque<org.openstack.swift.model.Object> list = new ArrayDeque<org.openstack.swift.model.Object>();
+			list.addAll(client.execute(command));
+			if (list.size() != limit) {
+				couldHaveMore = false;
+			}
+			return list;
+		}
 	}
 
 	@Override
