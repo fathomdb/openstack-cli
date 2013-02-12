9622a636e42298fc3ac2a928349477d4edb8a46e
Justin Santa Barbara <justin@fathomdb.com>
2013-02-12 12:08:22 -0800

    Introduce reflections API rather than our home-grown stuff
    Reflections can use a manifest, which is really cool

--- openstack-cli/src/main/java/org/openstack/client/cli/commands/OpenstackCliCommandRegistry.java
+++ openstack-cli/src/main/java/org/openstack/client/cli/commands/OpenstackCliCommandRegistry.java
@@ -1,11 +1,25 @@
 package org.openstack.client.cli.commands;
 
+import java.util.Set;
+
+import org.reflections.Reflections;
+
 import com.fathomdb.cli.commands.AutoComplete;
 import com.fathomdb.cli.commands.CommandRegistryBase;
 
 public class OpenstackCliCommandRegistry extends CommandRegistryBase {
 	public OpenstackCliCommandRegistry() {
 		addCommand(new AutoComplete());
-		discoverCommands(getClass().getPackage());
+
+		Reflections reflections = Reflections.collect();
+		// isProduction() ? Reflections.collect() : new
+		// Reflections(getClass().getPackage().getName());
+
+		Set<Class<? extends OpenstackCliCommandRunnerBase>> subTypes = reflections
+				.getSubTypesOf(OpenstackCliCommandRunnerBase.class);
+		discoverCommands(subTypes);
+
+		// discoverCommands(getClass().getPackage());
 	}
+
 }
--- openstack-cli/src/main/java/org/openstack/client/cli/output/OpenstackCliFormatterRegistry.java
+++ openstack-cli/src/main/java/org/openstack/client/cli/output/OpenstackCliFormatterRegistry.java
@@ -1,12 +1,26 @@
 package org.openstack.client.cli.output;
 
+import java.util.Set;
+
+import org.reflections.Reflections;
+
 import com.fathomdb.cli.formatter.FormatterRegistryBase;
+import com.fathomdb.cli.formatter.SimpleFormatter;
 
 public class OpenstackCliFormatterRegistry extends FormatterRegistryBase {
 
 	public OpenstackCliFormatterRegistry() {
 		addDefaultFormatters();
-		discoverFormatters(getClass().getPackage());
+
+		Reflections reflections = Reflections.collect();
+		// isProduction() ? Reflections.collect() : new
+		// Reflections(getClass().getPackage().getName());
+
+		Set<Class<? extends SimpleFormatter>> subTypes = reflections
+				.getSubTypesOf(SimpleFormatter.class);
+		discoverFormatters(subTypes);
+
+		// discoverFormatters(getClass().getPackage());
 	}
 
 }
